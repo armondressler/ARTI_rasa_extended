d114874071565f875b1b8efe1512166f706c893f
armon.dressler@nexxiot.com <armon.dressler@nexxiot.com>
2023-05-09 00:39:43 +0200

    working first version

--- actions/actions.py
+++ actions/actions.py
@@ -1,45 +1,38 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
 from typing import Any, Text, Dict, List
+import os
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()
+openai.api_key = os.environ.get('OPENAI_KEY')
+completion = openai.ChatCompletion()
+
 class InfosecQuiz(Action):
 
     def name(self) -> Text:
-        return "action_query_chatgpt"
+        return "infosec_follow_up"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text=f"You just told me: {tracker.latest_message['text']}")
+        question = tracker.latest_message['text']
+        answer = self.askgpt(question=question)
+        dispatcher.utter_message(text=f"You asked me: {tracker.latest_message['text']} and the answer is: {answer}")
 
         return []
+    
+    def askgpt(self, question, chat_log=None):
+        if chat_log is None:
+            chat_log = [{
+                'role': 'system',
+                'content': 'You are an it security expert',
+            }]
+        chat_log.append({'role': 'user', 'content': question})
+        response = completion.create(model='gpt-3.5-turbo', messages=chat_log)
+        answer = response.choices[0]['message']['content']
+        chat_log.append({'role': 'assistant', 'content': answer})
+        return answer
